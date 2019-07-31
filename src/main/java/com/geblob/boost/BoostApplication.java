0bf38c1aac9e6b6849a6f8680d61870378bf8b29
zhudahai <nextleave@gmail.com>
2019-07-31 13:55:55 +0800

    init boost.

---
+++ src/main/java/com/geblob/boost/BoostApplication.java
@@ -0,0 +1,13 @@
+package com.geblob.boost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoostApplication.class, args);
+    }
+
+}
