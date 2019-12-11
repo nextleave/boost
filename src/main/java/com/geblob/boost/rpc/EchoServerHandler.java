d8d0fb23bded4a18aa1028f293ba47a5cc449c7b
zhudahai <nextleave@gmail.com>
2019-12-12 00:06:05 +0800

    add hello controller

---
+++ src/main/java/com/geblob/boost/rpc/EchoServerHandler.java
@@ -0,0 +1,14 @@
+package com.geblob.boost.rpc;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("server received: " + in.toString());
+        ctx.write(in);
+    }
+}
---
+++ src/main/java/com/geblob/boost/rx/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.geblob.boost.rx;
+
+import io.reactivex.Flowable;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Flowable.just("Hello world");
+    }
+}
